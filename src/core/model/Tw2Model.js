b04d451f2530f3459142d2ed82cfd1900b29ebae
cppctamber <pauloosterman@gmail.com>
2019-12-26 10:50:19 +1300

    Fixed metadata in Tw2Model

--- src/core/model/Tw2Model.js
+++ src/core/model/Tw2Model.js
@@ -8,7 +8,7 @@ export class Tw2Model
     @meta.objectOf("Tw2GeometryModel")
     modelRes = null;
 
-    @meta.list("Tw2Bone")
+    @meta.listOf("Tw2Bone")
     bones = [];
 
     @meta.plainOf("Tw2Bone")
