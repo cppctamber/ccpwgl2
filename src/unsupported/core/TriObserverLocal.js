57ccdd49eb96ef1450b3116eab02dbe217ab1ed8
cppctamber <pauloosterman@gmail.com>
2020-06-09 22:52:39 +1200

    Updated black definitions for unsupported class TriObserverLocal

--- src/unsupported/core/TriObserverLocal.js
+++ src/unsupported/core/TriObserverLocal.js
@@ -6,7 +6,13 @@ import { meta, vec3, Tw2BaseClass } from "global";
 export class TriObserverLocal extends Tw2BaseClass
 {
 
+    @meta.black.string
+    name = "";
+
     @meta.black.vector3
     front = vec3.create();
+
+    @meta.black.object
+    observer = null;
 
 }
