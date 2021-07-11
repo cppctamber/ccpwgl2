3886e8c4866731be83600c79a09524dbf5cbae1f
cppctamber <pauloosterman@gmail.com>
2021-07-11 20:54:28 +1200

    Updated particle forces to inherit from older ones.

--- src/unsupported/eve/particle/EveParticleDirectForce.js
+++ src/unsupported/eve/particle/EveParticleDirectForce.js
@@ -1,23 +1,12 @@
 import { meta } from "utils";
-import { vec3 } from "math";
-import { Tw2ParticleForce } from "particle/force/Tw2ParticleForce";
+import { Tw2ParticleDirectForce } from "particle/force";
 
 /**
  * EveParticleDirectForce
  * Todo: Is this just a copy of Tw2ParticleDirectForce?
- *
- * @property {vec3} force -
  */
-export class EveParticleDirectForce extends Tw2ParticleForce
+@meta.type("EveParticleDirectForce")
+export class EveParticleDirectForce extends Tw2ParticleDirectForce
 {
-
-    @meta.vector3
-    force = vec3.create();
-
-    /**
-     * Identifies that the class is in staging
-     * @property {null|Number}
-     */
-    static __isStaging = 4;
 
 }
--- src/unsupported/eve/particle/EveParticleDragForce.js
+++ src/unsupported/eve/particle/EveParticleDragForce.js
@@ -1,22 +1,13 @@
-import { Tw2ParticleForce } from "particle/force/Tw2ParticleForce";
+import { Tw2ParticleDragForce } from "particle/force";
 import { meta } from "utils";
 
 /**
  * EveParticleDragForce
  * Todo: Is this just a copy of Tw2ParticleDragForce?
- *
- * @property {Number} drag -
  */
-export class EveParticleDragForce extends Tw2ParticleForce
+@meta.type("EveParticleDragForce")
+export class EveParticleDragForce extends Tw2ParticleDragForce
 {
 
-    @meta.float
-    drag = 0;
-
-    /**
-     * Identifies that the class is in staging
-     * @property {null|Number}
-     */
-    static __isStaging = 4;
 
 }
