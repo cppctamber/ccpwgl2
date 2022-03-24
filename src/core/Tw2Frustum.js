4dc63cc67f2958af9e242eb226113147e48c3ea7
cppctamber <pauloosterman@gmail.com>
2022-03-24 13:36:18 +1300

    Added helpers to Tw2Frustum - Added FromViewProjectionMatrix which sets the frustum from a view projection matrix - Added IntersectsPositionRadius - Added IntersectsSph3 - Added IntersectsBounds - Added IntersectsBox3 - Added ContainsPoint

--- src/core/Tw2Frustum.js
+++ src/core/Tw2Frustum.js
@@ -1,5 +1,5 @@
 import { meta } from "utils";
-import { vec3, vec4, mat4 } from "math";
+import { vec3, mat4, pln, box3 } from "math";
 
 
 @meta.type("Tw2Frustum")
@@ -7,10 +7,15 @@ export class Tw2Frustum
 {
 
     _halfWidthProjection = 1;
-    _planes = [ vec4.create(), vec4.create(), vec4.create(), vec4.create(), vec4.create(), vec4.create() ];
-    _viewPos = vec3.create();
-    _viewDir = vec3.create();
+    _viewPos = null;
+    _viewDir = null;
 
+    /**
+     * Frustum planes
+     * @type {Array<pln|vec4>}
+     * @private
+     */
+    _planes = [ pln.create(), pln.create(), pln.create(), pln.create(), pln.create(), pln.create() ];
 
     /**
      * Initializes the Tw2Frustum
@@ -25,69 +30,42 @@ export class Tw2Frustum
     {
         const mat4_0 = Tw2Frustum.global.mat4_0;
 
+        if (!this._viewPos) this._viewPos = new vec3.create();
+        if (!this._viewDir) this._viewDir = new vec3.create();
+
         const viewInv = viewInverse ? viewInverse : mat4.invert(mat4_0, view);
+
         this._viewPos.set(viewInv.subarray(12, 14));
         this._viewDir.set(viewInv.subarray(8, 10));
         this._halfWidthProjection = proj[0] * viewportSize * 0.5;
 
-        const
-            viewProj = viewProjection ? viewProjection : mat4.multiply(mat4_0, proj, view),
-            p = this._planes;
-
-        p[0][0] = viewProj[2];
-        p[0][1] = viewProj[6];
-        p[0][2] = viewProj[10];
-        p[0][3] = viewProj[14];
-
-        p[1][0] = viewProj[3] + viewProj[0];
-        p[1][1] = viewProj[7] + viewProj[4];
-        p[1][2] = viewProj[11] + viewProj[8];
-        p[1][3] = viewProj[15] + viewProj[12];
-
-        p[2][0] = viewProj[3] - viewProj[1];
-        p[2][1] = viewProj[7] - viewProj[5];
-        p[2][2] = viewProj[11] - viewProj[9];
-        p[2][3] = viewProj[15] - viewProj[13];
-
-        p[3][0] = viewProj[3] - viewProj[0];
-        p[3][1] = viewProj[7] - viewProj[4];
-        p[3][2] = viewProj[11] - viewProj[8];
-        p[3][3] = viewProj[15] - viewProj[12];
-
-        p[4][0] = viewProj[3] + viewProj[1];
-        p[4][1] = viewProj[7] + viewProj[5];
-        p[4][2] = viewProj[11] + viewProj[9];
-        p[4][3] = viewProj[15] + viewProj[13];
-
-        p[5][0] = viewProj[3] - viewProj[2];
-        p[5][1] = viewProj[7] - viewProj[6];
-        p[5][2] = viewProj[11] - viewProj[10];
-        p[5][3] = viewProj[15] - viewProj[14];
-
-        for (let i = 0; i < 6; ++i)
-        {
-            let len = vec3.length(p[i]);
-            p[i][0] /= len;
-            p[i][1] /= len;
-            p[i][2] /= len;
-            p[i][3] /= len;
-        }
+        const viewProj = viewProjection ? viewProjection : mat4.multiply(mat4_0, proj, view);
+        this.FromViewProjectionMatrix(viewProj);
     }
 
     /**
      * Checks to see if a sphere is visible within the frustum
-     *
      * @param {vec3} center
      * @param {Number} radius
      * @returns {Boolean}
      */
     IsSphereVisible(center, radius)
     {
-        const p = this._planes;
+        return this.IntersectsPositionRadius(center, radius);
+    }
 
+    /**
+     * Checks if the frustum intersects a sphere's components
+     * @param {vec3|Array} position
+     * @param {Number} radius
+     * @return {boolean}
+     */
+    IntersectsPositionRadius(position, radius)
+    {
+        const p = this._planes;
         for (let i = 0; i < 6; ++i)
         {
-            if (p[i][0] * center[0] + p[i][1] * center[1] + p[i][2] * center[2] + p[i][3] < -radius)
+            if (p[i][0] * position[0] + p[i][1] * position[1] + p[i][2] * position[2] + p[i][3] < -radius)
             {
                 return false;
             }
@@ -96,8 +74,65 @@ export class Tw2Frustum
     }
 
     /**
+     * Checks if the frustum intersects a sphere
+     * @param {sph3|Array} sph
+     * @return {boolean}
+     */
+    IntersectsSph3(sph)
+    {
+        return this.IntersectsPositionRadius(sph, sph[3]);
+    }
+
+    /**
+     * Checks if the frustum is intersected by a box
+     * @param {box3|Array} box
+     * @return {boolean}
+     */
+    IntersectsBox3(box)
+    {
+
+        const { vec3_0 } = this.constructor.global;
+
+        for (let i = 0; i < 6; i++)
+        {
+            const p = this._planes[i];
+            vec3_0[0] = p[0] > 0 ? box[0] : box[3];
+            vec3_0[1] = p[1] > 0 ? box[1] : box[4];
+            vec3_0[2] = p[2] > 0 ? box[2] : box[5];
+            if (pln.distanceToPoint(p, vec3_0) < 0) return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks if the frustum is intersected by a box's bounds
+     * @param {vec3} minBounds
+     * @param {vec3} maxBounds
+     * @return {boolean}
+     */
+    IntersectsBounds(minBounds, maxBounds)
+    {
+        return this.IntersectsBox3([ minBounds[0], minBounds[1], minBounds[2], maxBounds[0], maxBounds[1], maxBounds[2] ]);
+    }
+
+    /**
+     * Checks if the frustum contains a point
+     * @param {vec3|Array} v
+     * @return {boolean}
+     */
+    ContainsPoint(v)
+    {
+        const p = this._planes;
+        for (let i = 0; i < 6; i++)
+        {
+            if (pln.distanceToPoint(p[i], v) < 0) return false;
+        }
+        return true;
+    }
+
+    /**
      * GetPixelSizeAcross
-     *
      * @param {vec3} center
      * @param {Number} radius
      * @returns {Number}
@@ -114,6 +149,43 @@ export class Tw2Frustum
 
         let ratio = radius / depth;
         return ratio * this._halfWidthProjection * 2;
+    }
+
+    /**
+     * Sets the frustum from a projection matrix
+     * @param {mat4} m
+     * @return {Tw2Frustum}
+     */
+    FromViewProjectionMatrix(m)
+    {
+        const
+            planes = this._planes,
+            m0 = m[0],
+            m1 = m[1],
+            m2 = m[2],
+            m3 = m[3],
+            m4 = m[4],
+            m5 = m[5],
+            m6 = m[6],
+            m7 = m[7],
+            m8 = m[8],
+            m9 = m[9],
+            m10 = m[10],
+            m11 = m[11],
+            m12 = m[12],
+            m13 = m[13],
+            m14 = m[14],
+            m15 = m[15];
+
+
+        pln.setAndNormalize(planes[0], m3 - m0, m7 - m4, m11 - m8, m15 - m12);
+        pln.setAndNormalize(planes[1], m3 + m0, m7 + m4, m11 + m8, m15 + m12);
+        pln.setAndNormalize(planes[2], m3 + m1, m7 + m5, m11 + m9, m15 + m13);
+        pln.setAndNormalize(planes[3], m3 - m1, m7 - m5, m11 - m9, m15 - m13);
+        pln.setAndNormalize(planes[4], m3 - m2, m7 - m6, m11 - m10, m15 - m14);
+        pln.setAndNormalize(planes[5], m3 + m2, m7 + m6, m11 + m10, m15 + m14);
+
+        return this;
     }
 
     /**
