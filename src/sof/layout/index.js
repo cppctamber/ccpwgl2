69d4466351d90aaf09d5e2d0e5ae125fa7e1499d
cppctamber <pauloosterman@gmail.com>
2023-02-24 18:01:57 +1300

    Update black definitions

--- src/sof/layout/index.js
+++ src/sof/layout/index.js
@@ -1,3 +1,5 @@
 export * from "./EveSOFDataLayout";
 export * from "./EveSOFDataHullExtensionPlacement";
 export * from "./EveSOFDataHullExtensionPlacementDistributionMapGraphicSettings";
+export * from "./EveSOFDataHullExtensionPlacementDistributionParentMatch";
+export * from "./EveSOFDataHullExtensionPlacementDistributionPlacement";
