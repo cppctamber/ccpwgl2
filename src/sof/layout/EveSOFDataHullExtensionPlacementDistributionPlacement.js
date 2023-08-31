e9e4672a5f6a8b92a273baa422db384226942559
cppctamber <pauloosterman@gmail.com>
2023-08-31 22:30:28 +1200

    Update black definitions for EveSOFDataHullExtensionPlacementDistributionPlacement

--- src/sof/layout/EveSOFDataHullExtensionPlacementDistributionPlacement.js
+++ src/sof/layout/EveSOFDataHullExtensionPlacementDistributionPlacement.js
@@ -1,11 +1,29 @@
 import { meta } from "utils";
-
+import { vec3 } from "math";
 
 @meta.type("EveSOFDataHullExtensionPlacementDistributionPlacement")
 export class EveSOFDataHullExtensionPlacementDistributionPlacement extends meta.Model
 {
 
+    @meta.string
+    name = "";
+
+    @meta.float
+    centerBias = 0;
+
+    @meta.float
+    completeness = 0;
+
     @meta.list()
     distributionConditions = [];
+
+    @meta.vector3
+    placementBias = vec3.create();
+
+    @meta.vector3
+    randomScaleMin = vec3.create();
+
+    @meta.vector3
+    randomScaleMax = vec3.create();
 
 }
