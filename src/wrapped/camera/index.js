d6d56537e260c55a5e1631e546651d9ddfaaf1c2
paulo <pauloosterman@gmail.com>
2026-06-23 23:02:09 +1200

    chore: keep wrapped implementation out of tracked public files

--- src/wrapped/camera/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./OrthographicCamera";
-export * from  "./OrthoCamera";
-export * from "./PerspectiveCamera";
-export * from "./CameraOrbitControls";
-export * from "./OrbitControls";
--- src/wrapped/index.js
+++
@@ -1,6 +0,0 @@
-import { WrappedClient } from "./WrappedClient";
-
-export const tiny = new WrappedClient();
-
-
-
