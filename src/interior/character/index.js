cfb8d2c1732f487f5c80c3e558851767df3b8dc2
cppctamber <cppctamber@users.noreply.github.com>
2026-09-17 00:46:31 +1200

    Restore Carbon names for Tr2IntSkinnedObject, Tr2InteriorScene and Tr2GrannyStateRes

--- src/interior/character/index.js
+++ src/interior/character/index.js
@@ -1,7 +1,7 @@
 export * from "./CcpwglCharacterVisemeBinding";
 export * from "./Tw2InteriorAnimationController";
 export * from "./Tr2InteriorBoneOffset";
-export * from "./Tw2IntSkinnedObject";
+export * from "./Tr2IntSkinnedObject";
 export * from "./Tr2SkinnedModel";
 export * from "./Tw2CharacterMesh";
 
